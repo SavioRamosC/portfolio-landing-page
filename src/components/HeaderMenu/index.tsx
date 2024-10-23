ed886c986f6b50c2125d659add516cdca471e4f5
SavioRC <savio_rc@hotmail.com.br>
2024-10-23 16:08:12 -0300

    23-10-24

--- src/components/HeaderMenu/index.tsx
+++ src/components/HeaderMenu/index.tsx
@@ -8,10 +8,10 @@ export default function HeaderMenu({ isOpen }: MenuOptionsProps) {
   }
 
   return (
-    <ul className="w-64 flex flex-col absolute top-8 right-0 bg-colorRed border border-colorGray rounded-md shadow-md">
-      <li className="py-2 px-4 hover:bg-colorWhite rounded-t-md">My Account</li>
-      <li className="py-2 px-4 hover:bg-colorWhite ">Privacy</li>
-      <li className="py-2 px-4 hover:bg-colorWhite rounded-b-md">Settings</li>
+    <ul className="w-64 flex flex-col absolute top-8 right-0 bg-colorBlack border border-colorGray text-colorWhite rounded-md shadow-md">
+      <li className="py-2 px-4 hover:text-colorRed rounded-t-md">My Account</li>
+      <li className="py-2 px-4 hover:text-colorRed ">Privacy</li>
+      <li className="py-2 px-4 hover:text-colorRed rounded-b-md">Settings</li>
     </ul>
   );
 }
