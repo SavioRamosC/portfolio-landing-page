1378e35b76a270fed6ec7fcd2acf377db35d4ca7
SavioRC <savio_rc@hotmail.com.br>
2024-10-23 15:57:54 -0300

    23-10-24

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,18 +12,18 @@ export default function Header() {
   return (
     <header className="fixed w-full bg-colorBlack text-colorWhite py-3">
       <ul className="flex justify-center items-center">
-        <li className="md:ml-auto transition duration-300 text-colorWhite hover:text-colorRed">
-          <Link className="w-full h-full flex py-2 md:px-12 px-4 rounded-lg" href="/">
+        <li className="md:ml-auto transition duration-300  hover:text-colorRed">
+          <Link className="w-full h-full flex py-2 md:px-12 px-4 text-colorWhite rounded-lg" href="/">
             My Projects!
           </Link>
         </li>
-        <li className="md:m-auto transition duration-300 text-colorWhite hover:text-colorRed">
-          <Link className="w-full h-full flex py-2 md:px-12 px-4 rounded-lg" href="/">
+        <li className="md:m-auto transition duration-300  hover:text-colorRed">
+          <Link className="w-full h-full flex py-2 md:px-12 px-4 text-colorWhite rounded-lg" href="/">
             Home
           </Link>
         </li>
-        <li className="md:mr-auto transition duration-300 text-colorWhite hover:text-colorRed">
-          <Link className="w-full h-full flex py-2 md:px-12 px-4 rounded-lg" href="/about">
+        <li className="md:mr-auto transition duration-300  hover:text-colorRed">
+          <Link className="w-full h-full flex py-2 md:px-12 px-4 text-colorWhite rounded-lg" href="/about">
             About
           </Link>
         </li>
